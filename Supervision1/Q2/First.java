a09f54b7e7cf1eda7a9b2edd7a1e4d5a41523e70
danielvarley <dbv22@cam.ac.uk>
2014-11-17 18:18:16 +0000

    Create First.java

---
+++ Supervision1/Q2/First.java
@@ -0,0 +1,13 @@
+public class First {
+	public static void main(String[] args){
+		
+	// Primitive 
+		int y = 10;
+		System.out.println(y);
+	
+	// References 
+		int x = y;
+		System.out.println(x);
+
+	}
+}
