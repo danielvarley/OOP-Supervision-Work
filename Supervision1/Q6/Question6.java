2d5827f19cbc38f7037e7f59721e2caadcf738cc
danielvarley <dbv22@cam.ac.uk>
2014-11-17 18:21:24 +0000

    Create Question6.java

---
+++ Supervision1/Q6/Question6.java
@@ -0,0 +1,17 @@
+package Question6;
+
+public class Question6 {
+	
+	
+	public static void vectorAdd(int x,int y,int dx, int dy) { 
+		int x2=x+dx;
+		int y2=y+dy; 
+	}
+	public static void main(String[] args) { 
+		int a=0;
+		int b=2;
+		vectorAdd(a,b,1,1);
+		System.out.println(x2+" "+y2);
+	// (a,b) is still (0,2)
+	}
+}
